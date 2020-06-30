b1ca599c69fccfa1708ac1add8dc80e538fa5e04
kiseleff <zhenya-kiselev-97@mail.ru>
2020-06-30 14:33:07 +0300

    Добавление уведомлений. Возможность удалять уведомления, при удалении ячейки.

--- Birthday Tracker/AddBirthdayViewController.swift
+++ Birthday Tracker/AddBirthdayViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit;
 import CoreData
+import UserNotifications
 
 class AddBirthdayViewController: UIViewController {
 
@@ -43,6 +44,24 @@ class AddBirthdayViewController: UIViewController {
 // Сохранение:
         do{
             try context.save()
+            
+            let message = "Сегодня \(firstName) \(lastName) празднует день рождения!"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day],
+                                                                 from: birthdate)
+            dateComponents.hour = 8
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents,
+                                                        repeats: true)
+            if let identifier = newBirthday.birthdayId {
+                let request = UNNotificationRequest(identifier: identifier,
+                                                    content: content,
+                                                    trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
+            
         } catch let error {
             print("Не удалось сохранить из-за ошибки \(error).")
         }
--- Birthday Tracker/AppDelegate.swift
+++ Birthday Tracker/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+// Запрос разрешения на отправку уведомлений:
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound], completionHandler: {(granted, error) in
+            if granted {
+                print("Разрешение на отправку уведомлений получено!")
+            } else {
+                print("В разрешении на отправку уведомлений отказано.")
+            }
+        })
+        
         return true
     }
 
